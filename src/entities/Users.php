596bd17048f2e7d0050f1cb9991a314acf2fbb36
niemet0502 <vincentmarius8@gmail.com>
2021-02-19 17:12:52 +0100

    main :: added Users class content

---
+++ src/entities/Users.php
@@ -0,0 +1,37 @@
+<?php
+class Users extends Personne{
+  private $id;
+  private $email;
+  private $password;
+  
+  public function __construct($nom,$prenom,$id,$email,$password)
+  {
+    $this->nom = $nom;
+    $this->prenom = $prenom;
+    $this->id = $id;
+    $this->email = $email;
+    $this->password = $password;
+  }
+
+  //setters
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function setEmail($email){
+    $this->email = $email;
+  }
+  public function setPassword($password){
+    $this->password = $password;
+  }
+
+  //getters
+  public function getId(){
+    return $this->id;
+  }
+  public function getEmail(){
+    return $this->email;
+  }
+  public function getPassword(){
+    return $this->password;
+  }
+}
